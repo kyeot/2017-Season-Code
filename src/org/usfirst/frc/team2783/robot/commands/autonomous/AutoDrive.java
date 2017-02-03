8bea1bc58693094be7ef8d40a14a9580db94ae28
Owenatkins29 <owenatkins39@yahoo.com>
2017-02-02 20:50:32 -0500

    Added AutoDrive with Swerve
    Made the base command to autonomously drive swerve

---
+++ src/org/usfirst/frc/team2783/robot/commands/autonomous/AutoDrive.java
@@ -0,0 +1,64 @@
+package org.usfirst.frc.team2783.robot.commands.autonomous;
+
+import org.usfirst.frc.team2783.robot.Robot;
+import org.usfirst.frc.team2783.robot.subsystems.SwerveDriveBase.SwerveModule;
+
+import edu.wpi.first.wpilibj.Utility;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class AutoDrive extends Command {
+
+	private double fbMot;
+	private double lrMot;
+	private double rotMot;
+	private boolean fieldOriented;
+	private long commandStartedAt;
+	private double runTime;
+	
+    public AutoDrive(double fbMot, double lrMot, double rotMot, boolean fieldOriented, double runTime) {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.swerveBase);
+    	this.fbMot = fbMot;
+    	this.lrMot = lrMot;
+    	this.rotMot = rotMot;
+    	this.fieldOriented = fieldOriented;
+    	
+    	//Run Time is in Seconds
+    	this.runTime = runTime;  	
+    	
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	commandStartedAt = Utility.getFPGATime();
+       }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.swerveBase.swerveDrive(fbMot, lrMot, rotMot, fieldOriented);
+    	
+    	
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+    	//Run command for 6 seconds
+        return Utility.getFPGATime() > (runTime * 1000000 + commandStartedAt);
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	commandStartedAt = 0;
+    	Robot.swerveBase.swerveDrive(0, 0, 0, false);
+    	
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
