c9be22c39140a0d64dd4ce5c4625885dfd819d71
Owenatkins29 <owenatkins39@yahoo.com>
2017-02-03 20:13:46 -0500

    Added DriveTest Autonomous Command
    Added a command group to test the autonomous using AutoDrive

--- src/org/usfirst/frc/team2783/robot/Robot.java
+++ src/org/usfirst/frc/team2783/robot/Robot.java
@@ -1,13 +1,16 @@
 
 package org.usfirst.frc.team2783.robot;
 
+import org.usfirst.frc.team2783.robot.commands.autonomous.modes.DriveTest;
 import org.usfirst.frc.team2783.robot.subsystems.SwerveDriveBase;
+
 
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -22,6 +25,9 @@ public class Robot extends IterativeRobot {
 
 	public static final SwerveDriveBase swerveBase = new SwerveDriveBase();
 	public static OI oi;
+	public static Command autonomous;
+	public static NetworkTable smartDashTable;
+	
 
 	Command autonomousCommand;
 	SendableChooser<Command> chooser = new SendableChooser<>();
@@ -30,14 +36,17 @@ public class Robot extends IterativeRobot {
 	 * This function is run when the robot is first started up and should be
 	 * used for any initialization code.
 	 */
-	@Override
 	public void robotInit() {
 		oi = new OI();
 		// chooser.addObject("My Auto", new MyAutoCommand());
 		SmartDashboard.putData("Auto mode", chooser);
+		this.smartDashTable = NetworkTable.getTable("SmartDashboard");
 		
 		CameraServer usbCameraServer = CameraServer.getInstance();
 		usbCameraServer.startAutomaticCapture("cam0", 0);
+		
+		String[] autonomousList = {"DriveTest"};
+        this.smartDashTable.putStringArray("Auto List", autonomousList);
 	}
 
 	/**
@@ -68,18 +77,25 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void autonomousInit() {
-		autonomousCommand = chooser.getSelected();
-
-		/*
-		 * String autoSelected = SmartDashboard.getString("Auto Selector",
-		 * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
-		 * = new MyAutoCommand(); break; case "Default Auto": default:
-		 * autonomousCommand = new ExampleCommand(); break; }
-		 */
-
-		// schedule the autonomous command (example)
-		if (autonomousCommand != null)
-			autonomousCommand.start();
+    	
+    	//Gets the autonomous selector value from the dashboard
+    	String autoSelected = SmartDashboard.getString("Auto Selector", "None");
+    	
+    	//Switches the autonomous mode based on the value from the SmartDashboard
+		switch(autoSelected) {
+			case "DriveTest":
+				autonomous = new DriveTest();
+				break;
+			case "None":
+			default:
+				autonomous = null;
+				break;
+		} 
+		
+    	if(autonomous != null) {
+    		autonomous.start();
+    	}
+    	
 	}
 
 	/**
