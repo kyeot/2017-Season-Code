cfa3e2e99ccff7cccec25540865871a64190ef9f
Carl Yoder <carlyodes1@gmail.com>
2017-03-06 18:05:00 -0500

    Added Shooting Autonomous
    Shoots for 10s, then moves forward and rotates 180

---
+++ src/org/usfirst/frc/team2783/robot/commands/autonomous/modes/ShootFromStart.java
@@ -0,0 +1,38 @@
+package org.usfirst.frc.team2783.robot.commands.autonomous.modes;
+
+import org.usfirst.frc.team2783.robot.Robot;
+import org.usfirst.frc.team2783.robot.commands.GyroSwerveDrive;
+import org.usfirst.frc.team2783.robot.commands.autonomous.AutoDrive;
+import org.usfirst.frc.team2783.robot.commands.autonomous.AutoShoot;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class ShootFromStart extends CommandGroup {
+
+    public ShootFromStart() {
+    	
+    	requires(Robot.shooterBase);
+    	
+    	addSequential(new GyroSwerveDrive(180, 0.5, true, 0.5));
+    	addSequential(new AutoDrive(180, 0.5, 0.25, true, 1));
+        // Add Commands here:
+        // e.g. addSequential(new Command1());
+        //      addSequential(new Command2());
+        // these will run in order.
+
+        // To run multiple commands at the same time,
+        // use addParallel()
+        // e.g. addParallel(new Command1());
+        //      addSequential(new Command2());
+        // Command1 and Command2 will run in parallel.
+
+        // A command group will require all of the subsystems that each member
+        // would require.
+        // e.g. if Command1 requires chassis, and Command2 requires arm,
+        // a CommandGroup containing them would require both the chassis and the
+        // arm.
+    }
+}
