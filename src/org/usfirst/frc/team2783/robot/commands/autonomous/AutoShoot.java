af49ba261d031b22b631b8c380a84e2743a9d259
Carl Yoder <carlyodes1@gmail.com>
2017-03-06 18:06:51 -0500

    Fixed Auto Shoot
    Didnt set initial starting time, which immediately ended the method

--- src/org/usfirst/frc/team2783/robot/commands/autonomous/AutoShoot.java
+++ src/org/usfirst/frc/team2783/robot/commands/autonomous/AutoShoot.java
@@ -33,6 +33,7 @@ public class AutoShoot extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	commandStartedAt = Utility.getFPGATime();
        }
 
     // Called repeatedly when this Command is scheduled to run
