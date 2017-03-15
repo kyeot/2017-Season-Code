98a0998431b59990585354b0e754c61658251b31
Carl Yoder <carlyodes1@gmail.com>
2017-03-15 18:13:39 -0400

    Added Variable Climber Speed
    Made Right Trigger activate climber command to climb with a scalar
    speed.

--- src/org/usfirst/frc/team2783/robot/OI.java
+++ src/org/usfirst/frc/team2783/robot/OI.java
@@ -1,11 +1,13 @@
 package org.usfirst.frc.team2783.robot;
 
+import org.usfirst.frc.team2783.robot.commands.Climb;
 import org.usfirst.frc.team2783.robot.commands.GyroSwerveDrive;
 import org.usfirst.frc.team2783.robot.commands.RetrieveGear;
 import org.usfirst.frc.team2783.robot.commands.ShooterDrive;
 import org.usfirst.frc.team2783.robot.commands.ToggleRetriever;
 import org.usfirst.frc.team2783.robot.commands.VisionTrigger;
 import org.usfirst.frc.team2783.robot.subsystems.RetrieverClimberBase.RetrieverDirection;
+import org.usfirst.frc.team2783.robot.triggers.AxisButton;
 import org.usfirst.frc.team2783.robot.triggers.Dpad;
 import org.usfirst.frc.team2783.robot.vision.AdjustRotationToTarget;
 
@@ -22,13 +24,15 @@ public class OI {
 	
 	public static Joystick manipulator = new Joystick(RobotMap.MANIPULATOR_CONTROLLER_ID);
 	public static Joystick driver = new Joystick(RobotMap.XBOX_CONTROLLER_ID);
+	public static Joystick pidTuner = new Joystick(2);
 	
 	Button gearAuto = new JoystickButton(manipulator, 2);
 	Button shooter = new JoystickButton(manipulator, 8);
 	Button gatherer = new JoystickButton(manipulator, 6);
-	Button climber = new JoystickButton(manipulator, 5);
 	Button gearPlace = new JoystickButton(manipulator, 4);
 	Button retrieveGear = new JoystickButton(manipulator, 7);
+	
+	Button climber = new AxisButton(manipulator, 3);
 	
 	Button visionButton = new JoystickButton(driver, 1);
 	
@@ -37,21 +41,25 @@ public class OI {
 	Dpad gyroDriveEast = new Dpad(driver, 90);
 	Dpad gyroDriveWest = new Dpad(driver, 270);
 	
+	public static Dpad pValueUp = new Dpad(pidTuner, 0);
+	public static Dpad pValueDown = new Dpad(pidTuner, 180);
+	
 	VisionTrigger visionTrigger;
 
 	public OI() {
-		visionButton.whileActive(new AdjustRotationToTarget());
+		visionButton.toggleWhenPressed(new AdjustRotationToTarget());
 		retrieveGear.toggleWhenPressed(new RetrieveGear());
 		
 		shooter.toggleWhenPressed(new ShooterDrive());
 		
-		climber.whenPressed(new ToggleRetriever(RetrieverDirection.RET_OUT, 1));
+		climber.whenActive(new Climb());
+		
 		gatherer.whenPressed(new ToggleRetriever(RetrieverDirection.RET_OUT, 0));
 		
-		gyroDriveNorth.whileActive(new GyroSwerveDrive(0.0, 0.5, false));
-		gyroDriveSouth.whileActive(new GyroSwerveDrive(180.0, 0.5, false));
-		gyroDriveEast.whileActive(new GyroSwerveDrive(90.0, 0.5, false));
-		gyroDriveWest.whileActive(new GyroSwerveDrive(270.0, 0.5, false));
+		gyroDriveNorth.whileActive(new GyroSwerveDrive(0.0, 0.3, false));
+		gyroDriveSouth.whileActive(new GyroSwerveDrive(180.0, 0.3, false));
+		gyroDriveEast.whileActive(new GyroSwerveDrive(90.0, 0.3, false));
+		gyroDriveWest.whileActive(new GyroSwerveDrive(270.0, 0.3, false));
 	}
 
 	
