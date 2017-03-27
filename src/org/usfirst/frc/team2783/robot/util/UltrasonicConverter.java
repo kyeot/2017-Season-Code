5e973a21ace85df466d2f746bb612e6de46193d5
Calasada <carlyodes1@gmail.com>
2017-03-27 12:48:03 -0400

    Added UltrasonicConverter Util Class
    Takes analog input, getCentimeters converts that to centimeters with a
    conversion factor.

---
+++ src/org/usfirst/frc/team2783/robot/util/UltrasonicConverter.java
@@ -0,0 +1,25 @@
+package org.usfirst.frc.team2783.robot.util;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+
+public class UltrasonicConverter {
+
+	
+	AnalogInput analog;
+	
+	public UltrasonicConverter(int port){
+		this.analog = new AnalogInput(port);
+	}
+	
+	public double getCentimeters(){
+		return analog.getValue()/20;
+	}
+	
+	
+	//92.7
+	//132.08
+	
+	//39.38
+	
+	//
+}
