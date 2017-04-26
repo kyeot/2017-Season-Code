5c3a538de49fc66d63a5cc77265798fc150f56d4
Calasada <carlyodes1@gmail.com>
2017-04-25 23:55:42 -0400

    Changed ID's to Match Competition Bot
    Almost Identical to rocket-city with a few additions, also changed
    climber to a Talon.

--- src/org/usfirst/frc/team2783/robot/RobotMap.java
+++ src/org/usfirst/frc/team2783/robot/RobotMap.java
@@ -13,16 +13,16 @@ public class RobotMap {
 	final public static int MANIPULATOR_CONTROLLER_ID = 1;
 	
 	//Climber Base
-	final public static int CLIMBER_WHEEL_ID = 6;
+	final public static int CLIMBER_WHEEL_ID = 3;
 	
 	//Shooter Base
-	final public static int SHOOTER_WHEEL_ID = 5;
+	final public static int SHOOTER_WHEEL_ID = 4;
 	
 	//Swerve Base
-	final public static int FRONT_LEFT_WHEEL = 2;
-	final public static int FRONT_RIGHT_WHEEL = 4;
-	final public static int REAR_LEFT_WHEEL = 3;
-	final public static int REAR_RIGHT_WHEEL = 1;
+	final public static int FRONT_LEFT_WHEEL = 5;
+	final public static int FRONT_RIGHT_WHEEL = 1;
+	final public static int REAR_LEFT_WHEEL = 4;
+	final public static int REAR_RIGHT_WHEEL = 2;
 	
 	final public static int FRONT_LEFT_SWIVEL = 2;
 	final public static int FRONT_RIGHT_SWIVEL = 0;
@@ -30,7 +30,7 @@ public class RobotMap {
 	final public static int REAR_RIGHT_SWIVEL = 1;
 	
 	//Active Gear Base
-	final public static int GEAR_ROLLER_ID = 4;
+	final public static int GEAR_ROLLER_ID = 5;
 	final public static int GEAR_HOLDER_ID = 0;
 	
 	
